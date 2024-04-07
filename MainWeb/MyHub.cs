70d7702673ccdbe9c0c335a91ed0d3f50a379b9c
BingBing <BingBing@VOZGCHNNCQ>
2024-04-07 14:13:45 +0800

    添加简单的错误日志写出

--- MainWeb/MyHub.cs
+++ MainWeb/MyHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using MySqlX.XDevAPI;
 using Org.BouncyCastle.Tls;
 using System.Security.Cryptography;
@@ -14,21 +15,31 @@ namespace MainWeb
         private readonly DatabaseService _databaseService;
         private readonly List<string> fileNames = ["/www/toolserverscr/content-metadata", "/www/toolserverscr/content-document"];
         private readonly string Notice = "暫時沒有新通知";
+        private readonly ILogger<MyHub> _logger;
         public MyHub(DatabaseService databaseService)
         {
             _databaseService = databaseService ?? throw new ArgumentNullException(nameof(databaseService));
+            _logger = _logger ?? throw new ArgumentNullException(nameof(_logger));
         }
 
         // 当客户端连接时调用此方法
         public override async Task OnConnectedAsync()
         {
-            string connectionId = Context.ConnectionId;
+            try
+            {
+                string connectionId = Context.ConnectionId;
+                // 保存连接 ID 和用户标识符之间的映射关系
+                string userId = GetUserIdFromContext(Context);
+                _userConnectionMap[userId] = connectionId;
 
-            // 保存连接 ID 和用户标识符之间的映射关系
-            string userId = GetUserIdFromContext(Context);
-            _userConnectionMap[userId] = connectionId;
+                await base.OnConnectedAsync();
+            }
+            catch (Exception ex)
+            {
+                string logMessage = $"An error occurred: {ex.Message}{Environment.NewLine}{ex.StackTrace}";
+                WriteLogToFile("/www/toolserverlog.txt", logMessage);
+            }
 
-            await base.OnConnectedAsync();
         }
         private string GetUserIdFromContext(HubCallerContext context)
         {
@@ -70,60 +81,79 @@ namespace MainWeb
         // 客户端登录方法
         public async Task Login(string license, string deviceId)
         {
-            User user = await _databaseService.GetUserByLicenseKeyAsync(license);
-            //先判断卡密是否存在
-            if (user == null)
+            try
             {
-                await Clients.Caller.SendAsync("Error", "許可證不存在");
-                return;
-            }
-            //判断卡密是否过期
-            if (DateTime.UtcNow > user.ExpiryDate)
-            {
-                await Clients.Caller.SendAsync("Error", "許可證已經過有效期限");
-                return;
-            }
-            //判断卡密是否被使用
-            if (!user.IsUsed)
-            {   // 更新数据库中卡密状态为已使用，并将设备编号更新为客戶端傳入的设备编号
-                user.IsUsed = true;
-                user.DeviceId = deviceId;
-                await _databaseService.UpdateUserAsync(user);
-                //允許登錄
-                // 登录成功逻辑
-                await Clients.Caller.SendAsync("LoginSuccess", "登錄成功 到期時間:" + user.ExpiryDate);
-            }
-            //被使用過则进行设备号判断 是否一致
-            else if (user.DeviceId != deviceId)
-            {
-                // 设备号不匹配，進行判斷剩餘時間是否允許解綁
-                if (user.ExpiryDate.AddHours(-3) < DateTime.UtcNow)
+                User user = await _databaseService.GetUserByLicenseKeyAsync(license);
+                //先判断卡密是否存在
+                if (user == null)
                 {
-                    // 卡密剩余时间不足3小时，不允许登录
-                    await Clients.Caller.SendAsync("Error", "許可證時間不足解綁");
+                    await Clients.Caller.SendAsync("Error", "許可證不存在");
+                    return;
                 }
-                else
+                //判断卡密是否过期
+                if (DateTime.UtcNow > user.ExpiryDate)
                 {
-                    // 更新卡密的到期时间，减少3小时
-                    //user.ExpiryDate = user.ExpiryDate;
+                    await Clients.Caller.SendAsync("Error", "許可證已經過有效期限");
+                    return;
+                }
+                //判断卡密是否被使用
+                if (!user.IsUsed)
+                {   // 更新数据库中卡密状态为已使用，并将设备编号更新为客戶端傳入的设备编号
+                    user.IsUsed = true;
                     user.DeviceId = deviceId;
-                    user.ExpiryDate = user.ExpiryDate.AddHours(-3);
                     await _databaseService.UpdateUserAsync(user);
                     //允許登錄
                     // 登录成功逻辑
                     await Clients.Caller.SendAsync("LoginSuccess", "登錄成功 到期時間:" + user.ExpiryDate);
                 }
+                //被使用過则进行设备号判断 是否一致
+                else if (user.DeviceId != deviceId)
+                {
+                    // 设备号不匹配，進行判斷剩餘時間是否允許解綁
+                    if (user.ExpiryDate.AddHours(-3) < DateTime.UtcNow)
+                    {
+                        // 卡密剩余时间不足3小时，不允许登录
+                        await Clients.Caller.SendAsync("Error", "許可證時間不足解綁");
+                    }
+                    else
+                    {
+                        // 更新卡密的到期时间，减少3小时
+                        //user.ExpiryDate = user.ExpiryDate;
+                        user.DeviceId = deviceId;
+                        user.ExpiryDate = user.ExpiryDate.AddHours(-3);
+                        await _databaseService.UpdateUserAsync(user);
+                        //允許登錄
+                        // 登录成功逻辑
+                        await Clients.Caller.SendAsync("LoginSuccess", "登錄成功 到期時間:" + user.ExpiryDate);
+                    }
+                }
+                else
+                {
+                    // 登录成功逻辑
+                    await Clients.Caller.SendAsync("LoginSuccess", "登錄成功 到期時間:" + user.ExpiryDate);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                // 登录成功逻辑
-                await Clients.Caller.SendAsync("LoginSuccess", "登錄成功 到期時間:" + user.ExpiryDate);
+                string logMessage = $"An error occurred: {ex.Message}{Environment.NewLine}{ex.StackTrace}";
+                WriteLogToFile("/www/toolserverlog.txt", logMessage);
             }
+
         }
 
         public async Task GetNotice()
         {
-            await Clients.All.SendAsync("ReceiveNotice", Notice);
+            try
+            {
+
+                await Clients.All.SendAsync("ReceiveNotice", Notice);
+            }
+            catch (Exception ex)
+            {
+                string logMessage = $"An error occurred: {ex.Message}{Environment.NewLine}{ex.StackTrace}";
+                WriteLogToFile("/www/toolserverlog.txt", logMessage);
+                await Clients.Caller.SendAsync("Error", "未知錯誤！" + ex.Message);
+            }
         }
         public async Task RequestFiles(string license, string deviceId)
         {
@@ -165,8 +195,9 @@ namespace MainWeb
                         }
                         catch (Exception ex)
                         {
-                            Console.WriteLine(ex.Message);
-                            await Clients.Caller.SendAsync("Error", "未知錯誤！"+ ex.Message);
+                            string logMessage = $"An error occurred: {ex.Message}{Environment.NewLine}{ex.StackTrace}";
+                            WriteLogToFile("/www/toolserverlog.txt", logMessage);
+                            await Clients.Caller.SendAsync("Error", "未知錯誤！" + ex.Message);
                         }
                         finally
                         {
@@ -177,9 +208,12 @@ namespace MainWeb
                     await Task.WhenAll(tasks); // 等待所有文件请求完成
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                await Clients.Caller.SendAsync("Error", "未知錯誤！"+ ex.Message);
+
+                string logMessage = $"An error occurred: {ex.Message}{Environment.NewLine}{ex.StackTrace}";
+                WriteLogToFile("/www/toolserverlog.txt", logMessage);
+                await Clients.Caller.SendAsync("Error", "未知錯誤！" + ex.Message);
             }
         }
         /// <summary>
@@ -195,42 +229,26 @@ namespace MainWeb
             }
             catch (Exception ex)
             {
+
+                string logMessage = $"An error occurred: {ex.Message}{Environment.NewLine}{ex.StackTrace}";
+                WriteLogToFile("/www/toolserverlog.txt", logMessage);
                 await Clients.Caller.SendAsync("Error", "未知錯誤！");
                 return null;
             }
         }
-        private byte[] GenerateKeyFromSubscriptionAndDeviceID(string license, string deviceId)
+        private void WriteLogToFile(string filePath, string logMessage)
         {
-            string combinedString = license + deviceId;
-
-            // 使用 SHA256 计算哈希值
-            using (SHA256 sha256 = SHA256.Create())
+            try
             {
-                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(combinedString));
-                return hashBytes;
+                // 将日志消息写入文件
+                File.AppendAllText(filePath, logMessage + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                // 如果写入文件时发生异常，则记录异常并输出到控制台
+                Console.WriteLine("An error occurred while writing log to file: " + ex.Message);
             }
         }
-        private byte[] EncryptFile(byte[] fileBytes, byte[] key)
-        {
-            using (Aes aes = Aes.Create())
-            {
-                aes.Key = key;
-                aes.GenerateIV();
 
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    ms.Write(aes.IV, 0, aes.IV.Length);
-
-                    using (ICryptoTransform encryptor = aes.CreateEncryptor())
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-                    {
-                        cs.Write(fileBytes, 0, fileBytes.Length);
-                    }
-
-                    return ms.ToArray();
-                }
-            }
-        }
-        
     }
 }
